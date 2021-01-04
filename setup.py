cdd0b7fb93eccd7c2b0e9f28975451c1ea7590a4
Piet <piet.broemmel@gmail.com>
2021-01-04 18:24:30 +0100

    test setup

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ from distutils.core import setup
 setup(name='SVGsimplify',
       version='0.0.1',
       description='Tool to simplify svgs',
+      url='https://github.com/piebro/svgsimplify',
+      license='MIT',
       author='Piet Brömmel',
       author_email='piet.broemmel@gmail.com',
-      url='https://github.com/piebro/svgsimplify',
-      packages=['vpype', 'numpy', 'pickle', 'svgpathtools'],
+      install_requires=['vpype', 'numpy', 'pickle', 'svgpathtools'],
      )
